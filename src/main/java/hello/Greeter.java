ce4b77abba717227b4349589d39b3abd67b974b8
devopskroy <109326965+devopskroy@users.noreply.github.com>
2023-03-09 21:17:56 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project from Kishan Ray";
+        return "DevOps Project from Kishan Ray test line";
     }
 }
